9afd83ec40f259043ba1c9e86f2d607f996d28db
David Bryan Ramboy <davidbryanramboy@gmail.com>
2024-10-31 15:42:43 +0800

    update arguments

--- lib/mobile_app/docu_fields/document_file_field.dart
+++ lib/mobile_app/docu_fields/document_file_field.dart
@@ -10,7 +10,7 @@ class MFileForm extends ConsumerWidget {
   final String widgetName;
 
   // Widgets
-  final Future<void> Function(BuildContext, Size, WidgetRef, File?)
+  final Future<void> Function(BuildContext, Size, WidgetRef, List<int>?)
       showDocImageDialog;
 
   final Future<void> Function(BuildContext, Size, WidgetRef, String?)
@@ -44,8 +44,8 @@ class MFileForm extends ConsumerWidget {
   final void Function(bool) isDocuFileAlreadyPostedProvider;
 
   // Path file
-  final File? fileImgToPost;
-  final File? fileImgToPatch;
+  final List<int>? fileImgToPost;
+  final List<int>? fileImgToPatch;
 
   // Function
   final Future<void> Function() selectFile;
